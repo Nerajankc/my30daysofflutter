45c89dd3bb336cf0d43fa05035a65e93b0eecbbd
unknown <nirajankc360@gmail.com>
2021-05-16 06:41:51 +0545

    first comit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/login.dart';
+import 'pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      // themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/login": (context) => Login(),
+        "/": (context) => HomePage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Text("Homepage"),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
